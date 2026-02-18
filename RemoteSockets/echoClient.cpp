9c315c7664a85b151e572dfde72e536fa9cdc8c3
Pulkit Sharma <pulkitsharmam2609@gmail.com>
2026-02-18 06:43:45 +0000

    echoClient Updated and Tested, Working Successfully!

--- RemoteSockets/echoClient.cpp
+++ RemoteSockets/echoClient.cpp
@@ -10,6 +10,9 @@
 int main(){
 	int SocketFD = -1;
 	SocketFD = socket(AF_INET,SOCK_STREAM,0);
+	int Connect_ret=-1;
+	char Write_Buff[]="Hello from Client";
+	char Read_Buff[20];
     	if(SocketFD < 0){
         perror("Socket Creation Failed"); //perror is uesd instead of printf or cout so that we know what is the reason for failure
         return -1;
@@ -22,6 +25,26 @@ int main(){
 	ServerAddress.sin_family=AF_INET;
 	ServerAddress.sin_port=htons(9000);
 	ServerAddress.sin_addr.s_addr=inet_addr("127.0.0.1"); //servers address
+	socklen_t ServerAddressLength = sizeof(ServerAddress);
+	Connect_ret = connect(SocketFD,(struct sockaddr *)&ServerAddress,ServerAddressLength);
+	if(Connect_ret < 0){
+		perror("Server Connection Failed");
+		close(SocketFD);
+	}
+	std::cout<<"Connected to Server On IP : "<<inet_ntoa(ServerAddress.sin_addr)<<" PORT : "<<ntohs(ServerAddress.sin_port)<<"\n";
+
+	ssize_t ReadWriteBytes = write(SocketFD,&Write_Buff,sizeof(Write_Buff));
+	if(ReadWriteBytes <=0){
+		perror("Writing To Server Failed");
+	}
+	std::cout<<"Message Sent to Server Successfully\n";
+	
+	ReadWriteBytes = read(SocketFD,&Read_Buff,sizeof(Read_Buff));
+	if(ReadWriteBytes <= 0){
+		perror("Reading Data From Server Failed");
+	}
+	std::cout<<"Message Received From Server : \n";
+	std::cout.write(Read_Buff,ReadWriteBytes)<<"\n";
 	
 	close(SocketFD);
 	return 0;
